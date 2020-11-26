f33436227759698ab2ef87028abf9dedc02a2399
ishan0102 <ishan0102@gmail.com>
2020-11-26 12:59:44 -0600

    Add class for guest credentials

---
+++ src/finalproject/client/Credentials.java
@@ -0,0 +1,13 @@
+package finalproject.client;
+
+public class Credentials {
+    String name;
+    String email;
+    String password;
+
+    public Credentials(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
